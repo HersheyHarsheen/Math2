d5a1a18e76859bcdeac27172a6f13523dd913bfb
HersheyHarsheen <96706565+HersheyHarsheen@users.noreply.github.com>
2022-02-10 20:15:41 -0800

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,11 @@
+var player1 = localStorage.getItem("player-1");
+var player2 = localStorage.getItem("player-2");
+document.getElementById("player1_name").innerHTML = player1 + ":";
+document.getElementById("player2_name").innerHTML = player2 + ":";
+console.log(player1);
+document.getElementById("player_question").innerHTML = "Question turn:" + player1;
+document.getElementById("player_answer").innerHTML = "Answer turn:" + player2;
+var player1_score = 0;
+var player2_score = 0;
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
